2d3b031f351fccb60d84756d34899c26fae45787
Alberexxx <103864359+Alberexxx@users.noreply.github.com>
2023-12-05 12:17:39 -0300

    we

---
+++ public/js/hydr4/script.js
@@ -0,0 +1,188 @@
+
+// incio ----->
+const inicio_logo = ScrollReveal({reset: true})
+inicio_logo.reveal("#titulo3", {
+    origin: "top", 
+    distance: "100px", 
+    duration: 1000, 
+    delay: 1000, 
+    reset: false 
+});
+
+const inicio_bottom = ScrollReveal({reset: true})
+inicio_bottom.reveal("#titulo", {
+    origin: "bottom", 
+    distance: "60px", 
+    duration: 2000, 
+    delay: 0, 
+    reset: false 
+});
+
+const inicio_contato = ScrollReveal({reset: true})
+inicio_contato.reveal(".contato", {
+    origin: "bottom", 
+    distance: "0px", 
+    duration: 2000, 
+    delay: 1000, 
+    reset: false 
+});
+
+
+const inicio_img = ScrollReveal({reset: true})
+inicio_img.reveal("#img1", {
+    origin: "bottom", 
+    distance: "400px", 
+    duration: 4000, 
+    delay: 1400, 
+    reset: false 
+});
+
+const inicio_whats = ScrollReveal({reset: true})
+inicio_whats.reveal("#whatsapp", {
+    origin: "bottom", 
+    distance: "0px", 
+    duration: 1000, 
+    delay: 3500, 
+    reset: false 
+});
+
+// fim_inicio --->
+
+// pagina 02 --->
+const p2_titulo = ScrollReveal({reset: true})
+p2_titulo.reveal(".titulos1, .titulo1, #titulo_port", {
+    origin: "bottom", 
+    distance: "100px", 
+    duration: 1000, 
+    delay: 500, 
+    reset: false 
+});
+
+const p2_sub = ScrollReveal({reset: true})
+p2_sub.reveal(".sub_titulos", {
+    origin: "bottom", 
+    distance: "0px", 
+    duration: 2000, 
+    delay: 1000, 
+    reset: false 
+});
+
+const p2_linha = ScrollReveal({reset: true})
+p2_linha.reveal(".p2_linha", {
+    origin: "bottom", 
+    distance: "0px", 
+    duration: 2000, 
+    delay: 1700, 
+    reset: false 
+});
+
+const p2_text = ScrollReveal({reset: true})
+p2_text.reveal(".texto2, .texto1", {
+    origin:"left", 
+    distance: "60px", 
+    duration: 1000, 
+    delay: 1500, 
+    reset: false 
+});
+
+// fim pagina_2 --->
+
+// incio pagina_3 ---->
+
+const p3_bolinha = ScrollReveal({reset: true})
+p3_bolinha.reveal(".circulo", {
+    origin: "top", 
+    distance: "60px", 
+    duration: 2000, 
+    delay: 1500, 
+    reset: false 
+});
+
+
+const p3_linha = ScrollReveal({reset: true})
+p3_linha.reveal("#linha", {
+    origin: "top", 
+    distance: "0px", 
+    duration: 2100, 
+    delay: 5000, 
+    reset: false 
+});
+
+
+const p3_sub = ScrollReveal({reset: true})
+p3_sub.reveal(".text_titulo", {
+    origin: "right", 
+    distance: "0px", 
+    duration: 2000, 
+    delay: 2000, 
+    reset: false 
+});
+    
+
+const p3_text = ScrollReveal({reset: true})
+p3_text.reveal(".explicacao", {
+    origin: "right", 
+    distance: "0px", 
+    duration: 2500, 
+    delay: 2000, 
+    reset: false 
+});
+// fim --
+
+const p4_div_img_1 = ScrollReveal({reset: true})
+p4_div_img_1.reveal("#port_div1", {
+    origin: "left", 
+    distance: "10px", 
+    duration: 3000, 
+    delay: 500, 
+    reset: false 
+});
+
+const p4_div_img_2 = ScrollReveal({reset: true})
+p4_div_img_2.reveal("#port_div2", {
+    origin: "right", 
+    distance: "10px", 
+    duration: 3000, 
+    delay: 500, 
+    reset: false 
+});
+
+// fim pagina_4
+
+// inicio pagina_5
+
+const p5_logo = ScrollReveal({reset: true})
+p5_logo.reveal("#logo", {
+    origin: "left", 
+    distance: "0px", 
+    duration: 2000, 
+    delay: 500, 
+    reset: false 
+});
+
+const p5_linha = ScrollReveal({reset: true})
+p5_linha.reveal("#linha_footer", {
+    origin: "left", 
+    distance: "0px", 
+    duration: 2000, 
+    delay: 1000, 
+    reset: false 
+});
+
+const p5_redes = ScrollReveal({reset: true})
+p5_redes.reveal("#rede_icone", {
+    origin: "left", 
+    distance: "0px", 
+    duration: 2000, 
+    delay: 1500, 
+    reset: false 
+});
+
+const p5_botao = ScrollReveal({reset: true})
+p5_botao.reveal("#seta", {
+    origin: "left", 
+    distance: "0px", 
+    duration: 2000, 
+    delay: 1000, 
+    reset: false 
+});
